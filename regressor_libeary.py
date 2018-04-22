17c34c2082bff225223ae70c27ed986c4f05397c
Arijit Bag <arijitbag10@gmail.com>
2018-04-22 09:49:04 +0530

    Update regressor_libeary.py

--- regressor_libeary.py
+++ regressor_libeary.py
@@ -1,5 +1,4 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+
 """
 Created on Wed Feb 28 20:10:21 2018
 
@@ -14,14 +13,6 @@ dataset = pd.read_csv('Position_Salaries.csv')
 x = dataset.iloc[:, 1:2].values
 y = dataset.iloc[:, 2].values
 
-"""
-from sklearn.cross_validation import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
-
-from sklearn.preprocessing import StandardScaler
-sc_x = StandardScaler()
-x_train = sc_x.fit_transform(x_train)
-x_test = sc_x.transform(x_test)"""
 
 # create regressor here
 
